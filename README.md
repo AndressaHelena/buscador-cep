abcc79148cf2d244442d9c0f1b053a256d32e23c
Andressa Helena <handressa125@gmail.com>
2022-06-09 21:20:13 -0300

    Atualizando Readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Buscador de CEP
+
+Projeto desenvolvido em react com a finalidade de praticar os conceitos aprendido em aula. 
+
+## O que ele Faz?
+
+Ao digitar um CEP ele abre um card com infomações como:
+-Rua
+-Bairo
+-Cidade
+-Estado
+
+## Learn More
+
+Se o usuário digitar cep inválido sera apresentado um alerta 
+
+### Deployment
+
+
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import { BsSearch } from 'react-icons/bs'
+import api from './services/api'
+
+import './styles.css'
+
+
+function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+
+  async function handleSearch() {
+    if (input === '') {
+      alert("Preencha algum cep!")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("")
+
+    } catch {
+      alert("Ops erro ao buscar...Tente novamente.")
+      setInput("")
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <BsSearch size={25} color={'darkgray'} />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+
+        <main className="main">
+          <h2> CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade}  - {cep.uf}</span>
+        </main>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
